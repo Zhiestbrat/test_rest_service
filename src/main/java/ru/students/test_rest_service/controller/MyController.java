a40be64add88e113836acf2574d08fbe4d87d8eb
Zhiestbrat <fffcondakov@yandex.ru>
2022-10-14 02:34:17 +0500

    version 1.0

--- src/main/java/ru/students/test_rest_service/controller/MyController.java
+++ src/main/java/ru/students/test_rest_service/controller/MyController.java
@@ -1,8 +1,6 @@
 package ru.students.test_rest_service.controller;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,27 +8,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.students.test_rest_service.model.Request;
 import ru.students.test_rest_service.model.Response;
-import ru.students.test_rest_service.service.ModifyRequestService;
-import ru.students.test_rest_service.service.MyModifyService;
 
-@Slf4j
+
 @RestController
 public class MyController {
-
-    private final MyModifyService myModifyService;
-    private final ModifyRequestService modifyRequestService;
-
-    @Autowired
-    public MyController(@Qualifier("ModifyErrorMessage") MyModifyService myModifyService,
-                        ModifyRequestService modifyRequestService) {
-        this.myModifyService = myModifyService;
-        this.modifyRequestService = modifyRequestService;
-    }
 
     @PostMapping(value = "/feedback")
     public ResponseEntity<Response> feedback(@RequestBody Request request) {
 
-        log.info("Входящий request : " + String.valueOf(request));
+
 
         Response response = Response.builder()
                 .uid(request.getUid())
@@ -41,12 +27,7 @@ public class MyController {
                 .errorMessage("")
                 .build();
 
-        modifyRequestService.modifyRq(request);
 
-        Response responseAfterModify = myModifyService.modify(response);
-
-        log.info("Исходящий response : " + String.valueOf(response));
-
-        return new ResponseEntity<>(responseAfterModify, HttpStatus.OK);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
--- src/main/java/ru/students/test_rest_service/service/ModifyRequestService.java
+++
@@ -1,8 +0,0 @@
-package ru.students.test_rest_service.service;
-
-import ru.students.test_rest_service.model.Request;
-
-public interface ModifyRequestService {
-
-    void modifyRq(Request request);
-}
