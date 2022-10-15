8bc99f911cf4535920cb11c5a6ff9d6d58b82dd3
Zhiestbrat <fffcondakov@yandex.ru>
2022-10-15 23:51:33 +0500

    version 1.1

---
+++ src/main/java/ru/students/test_rest_service2/controller/MyController.java
@@ -0,0 +1,45 @@
+package ru.students.test_rest_service2.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import ru.students.test_rest_service2.model.Request;
+import ru.students.test_rest_service2.model.Response;
+import ru.students.test_rest_service2.service.MyModifyService;
+
+@Slf4j
+@RestController
+public class MyController {
+
+    private final MyModifyService myModifyService;
+
+    @Autowired
+    public MyController(@Qualifier("ModifySystemTime") MyModifyService myModifyService) {
+        this.myModifyService = myModifyService;
+    }
+
+    @PostMapping(value = "/feedback")
+    public ResponseEntity<Response> feedback(@RequestBody Request request) {
+
+        log.warn("Входящий request : " + String.valueOf(request));
+
+        Response response = Response.builder()
+                .uid(request.getUid())
+                .operationUid(request.getOperationUid())
+                .systemTime(request.getSystemTime())
+                .code("success")
+                .errorCode("")
+                .errorMessage("")
+                .build();
+
+        Response responseAfterModify = myModifyService.modify(response);
+        log.warn("Исходящий response : " + String.valueOf(response));
+
+        return new ResponseEntity<>(responseAfterModify, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/ru/students/test_rest_service2/service/MyModifyService.java
@@ -0,0 +1,9 @@
+package ru.students.test_rest_service2.service;
+
+
+import ru.students.test_rest_service2.model.Response;
+
+public interface MyModifyService {
+
+    Response modify(Response response);
+}
